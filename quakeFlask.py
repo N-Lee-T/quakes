021b0022cd62c09b2a0c32dcf7bc9a5bb5a9594f
NewStrongBox <nathan.lee.thompson@gmail.com>
2023-04-30 16:35:06 -0500

    Minor changes, and interactivity w/ HTTP GET requests

--- quakeFlask.py
+++ quakeFlask.py
@@ -5,24 +5,45 @@ import sys
  
 app = Flask(__name__)
 
-def get_lat_lon():
-    lat_lon = [43, -91]
-    return lat_lon
+# if we need to look up lat / lon from a location, fill in this function
+# currently just has dummy values
+# try: https://radar.com/content/alternatives/google-maps-api-vs-radar
+#
+# def get_lat_lon():
+    # """
+    # Gets the latitude and longitude for a given location, or the location if already given
+    # TO DO: implement getting coordinates for a given location
+
+    # ::param:: to be determined
+
+    # ::returns:: Latitude and longitude values in a 2-item list []
+    # """
+    # lat_lon = [43, -91]
+    # return lat_lon
 
 def get_quakes(coords, period): 
+    """
+    Gets data about earthquakes in the specificied time period from USGS, then determines nearest quake
 
-    # if period == 'h':
-    #     # within the last hour
-    #     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
-    #     time = 'last hour'
-    # elif period == 'd':
-    #     # all quakes within the last day
-    #     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
-    #     time = 'last 24 hours'
-    # else:
-    #     # all quakes within the last month - this takes a while...
-    #     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-    #     time = 'last month'
+    ::param coords:: A 2-item list of [latitude, longitude]
+    ::param period:: string - represention of time period for which quakes are to be fetched (can be 'h', 'd', or 'm')
+
+    ::returns::  JSON object containing three values: 'count' of total quakes
+                                                      'magnitude' of nearest quake
+                                                      'location' of nearest quake
+    """
+    if period == 'h':
+        # within the last hour
+        url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
+        time = 'last hour'
+    elif period == 'd':
+        # all quakes within the last day
+        url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+        time = 'last 24 hours'
+    else:
+        # all quakes within the last month - this takes a while...
+        url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+        time = 'last month'
 
     # w/o taking params
     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
@@ -39,8 +60,10 @@ def get_quakes(coords, period):
     count, minQuake = 0, sys.maxsize
     closest = None
     for quake in recent_quakes:
-        lat = coords[0] - int(quake["geometry"]["coordinates"][0]) 
-        lon = coords[1] - int(quake["geometry"]["coordinates"][1]) 
+        # print(quake["geometry"]["coordinates"], quake["geometry"]["coordinates"][0], type(quake["geometry"]["coordinates"][0]), int(quake["geometry"]["coordinates"][0]), type(int(quake["geometry"]["coordinates"][0])))
+        print(coords, coords[0], type(coords[0]))
+        lat = int(coords[0]) - int(quake["geometry"]["coordinates"][0]) 
+        lon = int(coords[1]) - int(quake["geometry"]["coordinates"][1]) 
         tot = abs(lat + lon) 
         if tot < minQuake:
             minQuake = tot
@@ -53,30 +76,43 @@ def get_quakes(coords, period):
     }
 
     # if returning JSON object, use this
-    # return(quake_facts)
+    return(quake_facts)
 
     # else, if rendering page, use this
-    return recent_quakes, count, closest, time
+    # return recent_quakes, count, closest, time
 
 @app.route("/", methods=["GET", "POST"])
-def yes():
+def start():
+    """
+    Simply renders the starting page
+    """
     return render_template('index.html')
 
 @app.route("/quakes", methods=["GET"])
-def hellyes():
-    period = request.args.get['time']
-    # coords[0], coords[1] = request.args.get['lat'], request.args.get['lon']
-    # may need to add functionality to get coordinates
-    coords = get_lat_lon()
+def get_those_quakes():
+    """
+    Calls methods to get coordinates and interact with USGS API
+    Example of URL string: 'http://localhost:5000/quakes?time=h&lat=43&lon=%2D91' - note %2D url encoding for ' - '
+                           'http://localhost:5000/quakes?time=h&lat=43&lon=65' - no URL encoding for negative value
+
+    ::returns:: JSON object if calling externally
+                Flask template if calling internally
+    """
+    period = request.args.get('time')
+
+    # coords = get_lat_lon()
+    coords = [None, None]
+    coords[0], coords[1] = request.args.get('lat'), request.args.get('lon')
 
     # send the data if using in other application
-    # quake = get_quakes
-    # return(json.dumps(quake))
+    quake = get_quakes(coords, period)
+    return(json.dumps(quake))
 
     # show the data if rendering webpage
-    recent_quakes, count, closest, time = get_quakes(coords, period)
-    return render_template('quakes.html', recent_quakes=recent_quakes, count=count, closest=closest, time=time)
+    # recent_quakes, count, closest, time = get_quakes(coords, period)
+    # return render_template('quakes.html', recent_quakes=recent_quakes, count=count, closest=closest, time=time)
 
 if __name__=='__main__':
     app.run(debug=True)
 
+## remember to check for negative latitudes and include %2D in URL string when implementing form!!
